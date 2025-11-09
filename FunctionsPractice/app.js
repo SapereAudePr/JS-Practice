3d57e6d3dc8d9ed4f8810723d4717c895103fc7b
SapereAudePr <capitolsmife@gmail.com>
2025-11-09 15:18:01 +0300

    Computer's choice is based on random number between 0 and 1 // winner is determined between if/else statements(comparisons of computer and player selections) // determine winner function takes playerSelection and computerSelection arguments to determine the winner

--- FunctionsPractice/app.js
+++ FunctionsPractice/app.js
@@ -4,8 +4,13 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 const DEFAULT_SELECTION = ROCK;
-
 const SELECTION_OPTIONS = [ROCK, PAPER, SCISSORS];
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WIN = "PLAYER_WIN";
+const RESULT_COMPUTER_WIN = "COMPUTER_WIN";
+
+
+let isGameRunning = false;
 
 const getPlayerChoice = function () {
     const userInput = prompt(`What is your choice: ${ROCK}, ${PAPER} or ${SCISSORS}`, "").toUpperCase();
@@ -16,7 +21,7 @@ const getPlayerChoice = function () {
             return userInput;
         },
         wrong: function () {
-            alert(`"${userInput}" is not a valid choice!   ` + `"${DEFAULT_SELECTION}" is selected by default!`);
+            alert(`"${userInput}" is not a valid choice! ` + `"${DEFAULT_SELECTION}" is selected by default!`);
             return DEFAULT_SELECTION;
         }
     }
@@ -25,8 +30,42 @@ const getPlayerChoice = function () {
     return userChoice ? isChoiceCorrect.correct() : isChoiceCorrect.wrong();
 }
 
+const getComputerChoice = function () {
+    const rndNumber = Math.random();
+
+    if (rndNumber < 0.34) {
+        return ROCK;
+    } else if (rndNumber < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+}
+
+const determineWinner = function (playerChoice, computerChoice) {
+    if (!isGameRunning) return;
+
+    if (playerChoice === computerChoice) {
+        return RESULT_DRAW;
+    } else if (playerChoice === PAPER && computerChoice === ROCK || playerChoice === SCISSORS && computerChoice === PAPER || playerChoice === ROCK && computerChoice === SCISSORS) {
+        return RESULT_PLAYER_WIN;
+    } else {
+        return RESULT_COMPUTER_WIN;
+    }
+}
+
+
 startGameBtn.addEventListener(`click`, function () {
+    if (isGameRunning) {
+        alert(`Game is already running!`);
+        return;
+    }
+    isGameRunning = true;
     console.log("Starting game...");
     const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    console.log(`Player's selection:  ${playerSelection}`);
+    const computerSelection = getComputerChoice();
+    console.log(`Computer's selection:  ${computerSelection}`);
+    const winner = determineWinner(playerSelection, computerSelection);
+    console.log(winner);
 });
