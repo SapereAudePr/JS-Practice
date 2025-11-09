4753823a9080fc19ac9e7b48a3fe87e9a77d1826
SapereAudePr <capitolsmife@gmail.com>
2025-11-09 12:27:19 +0300

    I've learned here that I can create functions in objects, in constants and create anonymus functions.

--- FunctionsPractice/app.js
+++ FunctionsPractice/app.js
@@ -1,8 +1,30 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const testGreetBtn = document.getElementById(`test-greet-btn`);
+
+const testEndBtn = document.getElementById(`test-end-btn`);
+
 function startGame() {
     alert(`Game starting!`);
-    console.log(startGameBtn);
+    console.log(alert);
 }
 
+const testFnc = {
+    greet: function(){
+        alert(`Greeting`);
+        console.log(`Greeting!`);
+    },
+    end: function(){
+        alert(`Ending!`);
+        console.log(`Ending!`);
+    }
+}
+
+
+
 startGameBtn.addEventListener('click', startGame);
+testGreetBtn.addEventListener('click', function (){
+    alert(`Greeting!`);
+    console.log(`Greeting!`);
+});
+testEndBtn.addEventListener('click', testFnc.end);
