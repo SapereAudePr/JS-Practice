0c07a1fc5077dc2a25fa03b2dcedba08103404a5
SapereAudePr <capitolsmife@gmail.com>
2025-11-09 11:57:03 +0300

    script reference in index.html fixed and a basic function for indirect call added

--- FunctionsPractice/app.js
+++ FunctionsPractice/app.js
@@ -1,1 +1,8 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+    alert(`Game starting!`);
+    console.log(startGameBtn);
+}
+
+startGameBtn.addEventListener('click', startGame);
