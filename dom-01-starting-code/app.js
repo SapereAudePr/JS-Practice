049f261406c20a9796a9217fafe846e89cea2de3
SapereAudePr <capitolsmife@gmail.com>
2025-11-17 15:52:29 +0300

    Little bit of practice of what've learned

--- dom-01-starting-code/app.js
+++ dom-01-starting-code/app.js
@@ -1,17 +1,37 @@
-// const listItemElements = document.querySelectorAll('li');
+const button = document.querySelector('button');
 
-const lisItemElements = document.getElementsByTagName('li');
-// const lisItemIndex = lisItemElements[0];
-// lisItemIndex.textContent = 'Item99';
+const section = document.querySelector('section');
 
-const li = document.querySelector('li:last-of-type');
-li.textContent = li.textContent + `(999)`;
+const li = document.querySelectorAll('li');
 
-const li2 = document.querySelector('ul li:nth-child(2)');
-li2.textContent =  `ItemTEsst`
+const ul = document.querySelector('ul');
 
+for (let i = 0; i < li.length; i++) {
+    if (li[i].firstElementChild !== ``) {
+        li[i].textContent = `ItemChanged`;
+    }
 
-for (const lisItem of lisItemElements) {
-    if (lisItem.textContent === `Item1`) lisItem.textContent = `Item99`;
-    console.dir(lisItem);
+    li[i].insertAdjacentHTML('afterbegin', `<li>ItemTest</li>`);
 }
+
+const newLi = document.createElement('li');
+
+ul.appendChild(newLi);
+
+newLi.textContent = `AppendedChild`;
+
+section.className = `yellow-bg`;
+
+button.addEventListener('click', e => {
+    // if (section.className === 'yellow-bg visible') {
+    //     section.className = 'yellow-bg invisible';
+    // } else {
+    //     section.className = 'yellow-bg visible';
+    // }
+    section.insertAdjacentHTML('beforeend', '<h1>Item4</h1>');
+    if (section.className === 'yellow-bg') {
+    section.className = 'green-bg';
+    } else if (section.className === 'green-bg') {
+        section.className = ('yellow-bg');
+    }
+});
