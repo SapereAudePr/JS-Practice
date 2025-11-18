a6e8ae6124fb7570d95f31183e7a9879705554b3
SapereAudePr <capitolsmife@gmail.com>
2025-11-18 18:30:02 +0300

    Movie adding logic has been built.

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,8 +1,25 @@
+const modalActions = document.querySelector('.modal__actions');
+
+const modalActionCancelBtn = modalActions.querySelector('.btn--passive');
+const modalActionAddBtn = modalActions.querySelector('.btn--success');
+
 const addModal = document.getElementById('add-modal');
 
 const startAddBtn = document.querySelector('header button');
 
 const backDrop = document.body.firstElementChild;
+
+const userInputs = addModal.querySelectorAll('input');
+
+const section = document.querySelector('#entry-text');
+
+const movies = [];
+
+
+const updateUI = () => section.classList.toggle(`invisible`, movies.length > 0);
+
+
+
 
 const toggleBackDrop = () => {
     backDrop.classList.toggle('visible');
@@ -11,6 +28,42 @@ const toggleBackDrop = () => {
 const toggleMovieModal = () => {
     addModal.classList.toggle('visible');
     toggleBackDrop();
+    clearMovieInputs();
+}
+
+const toggleCancelModalHandler = () => {
+    toggleMovieModal();
+}
+
+const addMovieHandler = () => {
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if (titleValue.trim() === '' || imageUrlValue.trim() === '' || ratingValue.trim() === '' || +ratingValue < 1 || +ratingValue > 5) {
+        alert('Please enter a valid input');
+
+    }
+
+    const newMovie = {
+        title: titleValue,
+        imageUrl: imageUrlValue,
+        rating: ratingValue,
+    }
+
+    movies.push(newMovie);
+    console.log(movies);
+    toggleMovieModal();
+    updateUI();
+}
+
+const clearMovieInputs = () => {
+    for (const input of userInputs) {
+        input.value = '';
+    }
 }
 
 startAddBtn.addEventListener('click', toggleMovieModal);
+backDrop.addEventListener('click', toggleCancelModalHandler);
+modalActionCancelBtn.addEventListener('click', toggleCancelModalHandler);
+modalActionAddBtn.addEventListener(`click`, addMovieHandler)
