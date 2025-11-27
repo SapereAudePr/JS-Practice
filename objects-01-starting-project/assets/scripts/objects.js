4667133413ddf778c72849e3809ed951350ce9d7
SapereAudePr <capitolsmife@gmail.com>
2025-11-27 23:54:11 +0300

    ///

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -1,51 +1,50 @@
-const addMovieBtn = document.getElementById(`add-movie-btn`);
-const searchMovieBtn = document.getElementById(`search-btn`);
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchMovieBtn = document.getElementById("search-movie-btn");
 
-
+const movieList = document.getElementById("movie-list");
 const movies = [];
 
-const renderMovies = () => {
-    const movieList = document.getElementById('movies-list');
+const renderMovies = (title, genre, comment, point, id) => {
+    const createList = document.createElement("li");
 
-    // movies.length === 0 ? movieList.classList.remove('visible') : movieList.classList.add('visible');
+    createList.innerHTML = `
+    <div style="color: blue">${title} </div>
+    <div style="color: #f3cc4b">${genre}</div>
+    <div style="color: greenyellow">${comment}</div>
+    <div>${point}</div>`
 
-    // if (movies.length === 0) {
-    //     movieList.classList.remove('visible');
-    //     return;
-    // } else {
-    //     movieList.classList.add('visible');
-    // }
+    movies.length > 0 ? movieList.classList.add(`visible`) : movieList.classList.remove(`visible`);
 
-    movieList.innerHTML = '';
-
-    movies.forEach((movie) => {
-        const movieEl = document.createElement('li');
-        movieEl.textContent = movie.info.title;
-        movieList.append(movieEl);
-    })
+    movieList.append(createList);
 }
 
 const addMovieHandler = () => {
-    const title = document.getElementById('title').value;
-    const extraName = document.getElementById('extra-name').value;
-    const extraValue = document.getElementById('extra-value').value;
+    const title = document.getElementById("title").value;
+    const genre = document.getElementById("genre").value;
+    const comment = document.getElementById("comment").value;
+    const point = document.getElementById("point").value;
 
-    if (title.trim() === `` || extraName.trim() === `` || extraValue.trim() === ``) {
-        return
-    }
+    if (title.trim() === "" || genre.trim() === "" || comment.trim() === "") return;
 
     const newMovie = {
-        info: {
-            title,
-            [extraName]: extraValue
-        },
-        id: Math.random()
+        title,
+        genre,
+        comment,
+        point,
+        id: Math.random(),
     }
 
     movies.push(newMovie);
     console.log(movies);
-    renderMovies();
+    console.log(newMovie);
+    renderMovies(newMovie.title, newMovie.genre, newMovie.comment, newMovie.point, newMovie.id);
+}
+
+const searchMovieHandler = () => {
+    const filterTerm = document.getElementById("filter-title").value;
+    renderMovies(filterTerm);
 }
 
 
-addMovieBtn.addEventListener('click', addMovieHandler);
+addMovieBtn.addEventListener("click", addMovieHandler);
+searchMovieBtn.addEventListener("click", searchMovieHandler);
