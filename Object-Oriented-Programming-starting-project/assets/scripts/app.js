82cbfc54648c51804f0ddf0664f8e5174ace1f55
SapereAudePr <capitolsmife@gmail.com>
2025-12-02 03:26:52 +0300

    Now there's three classes to make this product adding process easier and cleaner with OOP

--- Object-Oriented-Programming-starting-project/assets/scripts/app.js
+++ Object-Oriented-Programming-starting-project/assets/scripts/app.js
@@ -1,9 +1,4 @@
 class Product {
-    title;
-    imageUrl;
-    price;
-    description;
-
     constructor(title, image, price, desc) {
         this.title = title;
         this.imageUrl = image;
@@ -12,34 +7,50 @@ class Product {
     }
 }
 
-const productList = {
-    products: [
+class ProductItem {
+    constructor(product) {
+        this.product = product;
+    }
+
+    render() {
+        const prodEl = document.createElement('li');
+        prodEl.className = `product-item`;
+        prodEl.innerHTML = `
+                <div>
+                <img src="${this.product.imageUrl}" alt="${this.product.title}">
+                    <div class="product-item__content">
+                            <h2>${this.product.title}</h2>
+                            <h3>\$${this.product.price}</h3>
+                            <p>${this.product.description}</p>
+                            <button>Add to cart</button>
+                    </div>
+                </div>
+            `;
+        return prodEl;
+    }
+}
+
+class ProductList {
+    products = [
         new Product('Forest', `https://greggvanourek.com/wp-content/uploads/2023/08/Nature-path-by-water-trees-and-mountains-AdobeStock_291242770-scaled.jpeg`, 2000, `Nature Path by water and trees`),
         new Product(`Scene`, `https://greggvanourek.com/wp-content/uploads/2023/08/Nature-path-by-water-trees-and-mountains-AdobeStock_291242770-scaled.jpeg`, 6000, `Beautiful lake around the mountains`),
-    ],
+    ];
+
+    constructor() {}
+
     render() {
         const renderHook = document.getElementById(`app`);
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for (const prod of this.products) {
-            const prodEl = document.createElement('li');
-            prodEl.className = `product-item`;
-            prodEl.innerHTML = `
-                <div>
-                <img src="${prod.imageUrl}" alt="${prod.title}">
-                    <div class="product-item__content">
-                            <h2>${prod.title}</h2>
-                            <h3>\$${prod.price}</h3>
-                            <p>${prod.description}</p>
-                            <button>Add to cart</button>
-                    </div>
-                </div>
-            `;
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
     }
 }
-productList.render();
 
+const productList = new ProductList();
+productList.render();
 
