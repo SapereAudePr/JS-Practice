fc13576558f88eab2abaff764eceb34185a49b3e
SapereAudePr <capitolsmife@gmail.com>
2025-12-02 05:07:25 +0300

    Nothing renders rn and I'm trying to find the bug

--- Object-Oriented-Programming-starting-project/assets/scripts/app.js
+++ Object-Oriented-Programming-starting-project/assets/scripts/app.js
@@ -7,28 +7,71 @@ class Product {
     }
 }
 
-class ShoppingCart {
+class ElementAttribute {
+    constructor(attrName, attrValue) {
+        this.name = attrName;
+        this.value = attrValue;
+    }
+}
+
+class Component {
+    constructor(renderHookId) {
+        this.hookId = renderHookId;
+        this.render();
+    }
+
+    render() {}
+
+    createRootElement(tag, cssClasses, attributes) {
+        const rootElement = document.createElement(tag);
+        if (cssClasses) {
+            rootElement.className = cssClasses;
+        }
+        if (attributes && attributes.length > 0) {
+            for (const attribute of attributes) {
+                rootElement.setAttribute(attribute.name, attribute.value);
+            }
+        }
+        document.getElementById(this.hookId).append(rootElement);
+        return rootElement;
+    }
+}
+
+class ShoppingCart extends Component {
     items = [];
 
+    set cartItems(value) {
+        this.items = value;
+        this.totalOutput.innerHTML = `<h2>Total Amount: \$${this.totalAmount.toFixed(2)}</h2>`;
+    }
+
+    get totalAmount() {
+        return this.items.reduce((prevValue, curItem) => prevValue + curItem.price, 0);
+    }
+
+    constructor(renderHookId) {
+        super(renderHookId);
+    }
+
     addProduct(product) {
-        this.items.push(product);
-        this.totalOutput.innerHTML = `<h2>Total Amount: \$${1}</h2>`;
+        const updatedItems = [...this.items];
+        updatedItems.push(product);
+        this.cartItems = updatedItems;
     }
 
     render() {
-        const cartEl = document.createElement(`section`);
+        const cartEl = this.createRootElement(`section`, `cart`)
         cartEl.innerHTML = `
         <h2>Total Amount: \$${0}</h2>
         <button>Order Now!</button>
         `;
-        cartEl.className = `cart`;
         this.totalOutput = cartEl.querySelector(`h2`);
-        return cartEl;
     }
 }
 
-class ProductItem {
-    constructor(product) {
+class ProductItem extends Component {
+    constructor(product, renderHookId) {
+        super(renderHookId);
         this.product = product;
     }
 
@@ -37,8 +80,7 @@ class ProductItem {
     }
 
     render() {
-        const prodEl = document.createElement('li');
-        prodEl.className = `product-item`;
+        const prodEl = this.createRootElement(`li`, `product-item`);
         prodEl.innerHTML = `
                 <div>
                 <img src="${this.product.imageUrl}" alt="${this.product.title}">
@@ -52,43 +94,32 @@ class ProductItem {
             `;
         const addCartBtn = prodEl.querySelector('button');
         addCartBtn.addEventListener('click', this.addToCart.bind(this));
-        return prodEl;
     }
 }
 
-class ProductList {
+class ProductList extends Component {
     products = [
         new Product('Forest', `https://greggvanourek.com/wp-content/uploads/2023/08/Nature-path-by-water-trees-and-mountains-AdobeStock_291242770-scaled.jpeg`, 2000, `Nature Path by water and trees`),
         new Product(`Scene`, `https://greggvanourek.com/wp-content/uploads/2023/08/Nature-path-by-water-trees-and-mountains-AdobeStock_291242770-scaled.jpeg`, 6000, `Beautiful lake around the mountains`),
     ];
 
-    constructor() {
+    constructor(renderHookId) {
+        super(renderHookId);
     }
 
     render() {
-
-        const prodList = document.createElement('ul');
-        prodList.className = 'product-list';
+        this.createRootElement(`ul`, `product-list`, [new ElementAttribute(`id`, `prod-list`)]);
         for (const prod of this.products) {
-            const productItem = new ProductItem(prod);
-            const prodEl = productItem.render();
-            prodList.append(prodEl);
+            new ProductItem(prod, `prod-list`);
         }
-        return prodList;
     }
 }
 
 
 class Shop {
     render() {
-        const renderHook = document.getElementById(`app`);
-        this.cart = new ShoppingCart();
-        const cartEl = this.cart.render();
-        const productList = new ProductList();
-        const prodListEl = productList.render();
-
-        renderHook.append(cartEl);
-        renderHook.append(prodListEl);
+        this.cart = new ShoppingCart(`app`);
+        new ProductList(`app`);
     }
 }
 
@@ -98,7 +129,6 @@ class App {
 
     static init() {
         const shop = new Shop();
-        shop.render();
         this.cart = shop.cart;
     }
 
@@ -108,6 +138,4 @@ class App {
 }
 
 App.init();
-
-
 
