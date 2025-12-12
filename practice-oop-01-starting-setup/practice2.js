c3f557dfa4ae0a8630fc3942dfd1b19e25cace04
SapereAudePr <capitolsmife@gmail.com>
2025-12-12 14:35:44 +0300

    One challange is passed

--- practice-oop-01-starting-setup/practice2.js
+++ practice-oop-01-starting-setup/practice2.js
@@ -1,63 +1,36 @@
-class Customer {
+class Student {
+    constructor(name, teacher) {
+        this.name = name;
+        this.teacher = teacher;
+        console.log(teacher);
+    }
+
+    study() {
+        console.log(`Studying: ${this.name}`);
+        this.teacher.teach(this);
+    }
+}
+
+class Teacher {
     constructor(name) {
         this.name = name;
     }
 
-    orderBooks(orderService, bookName) {
-        return orderService.createOrder(this, bookName);
-    }
-}
-
-
-class BookStore {
-    constructor(books) {
-        this.books = books;
-    }
-
-    getPrice(bookName) {
-        const price = this.books[bookName];
-        return price === undefined ? null : price;
-    }
-}
-
-
-class OrderService {
-    constructor(bookStore, logger) {
-        this.bookStore = bookStore;
-        this.logger = logger;
-    }
-
-    createOrder(customer, bookName) {
-        const bookPrice = this.bookStore.getPrice(bookName);
-        const customerName = customer.name;
-        this.logger.log(`Creating order with customer: ${customerName}, bookName: ${bookName} and book price: ${bookPrice}`);
-    }
-}
-
-class Logger {
-    log(text) {
-        console.log(`[LOG]${text}`);
+    teach(student) {
+        console.log(`${this.name} is teaching ${student.name}`);
     }
 }
 
 class App {
     constructor() {
-        const bookList = {
-            "Dune": 60,
-            "TheMartian": 80,
-        }
-
-        this.bookStore = new BookStore(bookList);
-        this.logger = new Logger();
-        this.orderService = new OrderService(this.bookStore, this.logger);
-        this.customer = new Customer(`Raven`);
+        this.teacher = new Teacher(`John Doe`);
+        this.student = new Student(`Raven`, this.teacher);
     }
 
-    init() {
-        const order1 = this.customer.orderBooks(this.orderService, `Dune`);
-        const order2 = this.customer.orderBooks(this.orderService, `TheMartian`);
+    run() {
+        this.student.study();
     }
 }
 
 const app = new App();
-app.init();
+app.run();
