d529a0fafc9aadba50e1cc3d38fb705301063472
SapereAudePr <capitolsmife@gmail.com>
2025-12-13 15:39:15 +0300

    I've built the base for the challange

--- practice-oop-01-starting-setup/practice2.js
+++ practice-oop-01-starting-setup/practice2.js
@@ -1,88 +1,77 @@
-class Player {
-    constructor(name, enemy, logger) {
+class User {
+    constructor(name, borrowedBooks = []) {
         this.name = name;
-        this.enemy = enemy;
-        this.health = 100;
-        this.logger = logger;
+        this.borrowedBooks = borrowedBooks;
     }
 
-    attack() {
-        if (this.enemy.health <= 0) return;
-        const rndNum = Math.floor(Math.random() * 30);
-        this.enemy.takeDamage(rndNum);
-        this.logger.log(`Outgoing damage to ${this.enemy.name} is: ${rndNum}`)
+    borrowBook(book) {
+
     }
 
-    takeDamage(amount) {
-        this.health -= amount;
-        this.logger.log(`${this.name} health after damage: ${this.health}`);
+    returnBook(book) {
 
-        if (this.health <= 0) {
-            this.logger.log(`${this.name} is dead!`)
-        }
     }
 }
 
-class Enemy {
-    constructor(name, player, logger) {
-        this.name = name;
-        this.logger = logger;
-        this.health = 100;
-        this.player = player;
+
+class Book {
+    constructor(title, isBorrowed = false, borrowedBy = null) {
+        this.title = title;
+        this.isBorrowed = isBorrowed;
+        this.borrowedBy = borrowedBy;
     }
 
-    attack() {
-        const rndNum = Math.floor(Math.random() * 30);
-        this.player.takeDamage(rndNum);
-        this.logger.log(`Outgoing damage to the ${this.player.name} is: ${rndNum}`);
+    borrowBook(book) {
+
     }
 
-    takeDamage(amount) {
-        this.health -= amount;
-        this.logger.log(`${this.name} health after damage: ${this.health}`);
+    returnBook() {
 
-        if (this.health <= 0) {
-            this.logger.log(`${this.name} is dead!`);
-            return;
-        }
+    }
+}
 
-        this.attack();
+class Library {
+    constructor(books = [], users = []) {
+        this.books = books;
+        this.users = users;
+    }
+
+    addBook(book) {
+
+    }
+
+    addUser(user) {
+
+    }
+
+    listAvailableBooks() {
+
     }
 }
 
 class Logger {
-    constructor(startingIndex = 0) {
-        this.index = startingIndex;
+    constructor() {
+        this.index = 0;
     }
 
     log(text) {
         this.index++;
-        console.log(`[${this.index}] [LOG] ${text}`);
+        console.log(`[LOG] -- [${this.index}] ${text}`);
     }
 }
 
 class App {
     constructor() {
-        this.logger = new Logger();
-        this.player = new Player(`Hero`, null, this.logger);
-        this.enemy = new Enemy(`Ogre`, this.player, this.logger);
-        this.player.enemy = this.enemy;
+        this.book = new Book();
+        this.library = new Library();
+        this.user = new User();
     }
 
     run() {
-        let turn = 1;
-        while (this.player.health > 0 && this.enemy.health > 0) {
-            this.player.attack();
-            turn++;
-            this.logger.log(`$$$$$$$$$[TURN] --${turn}--  $$$$$$$$$$`);
 
-            if (turn > 100) {
-                this.logger.log(`Turn is reached out it's limit: ${turn}`)
-                break;
-            }
-        }
     }
 }
 
 const app = new App();
 app.run();
+
