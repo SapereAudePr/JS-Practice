924c2b06051f6a8f716793461a9bfcb15811511d
SapereAudePr <capitolsmife@gmail.com>
2025-12-13 16:12:14 +0300

    I've added books and users

--- practice-oop-01-starting-setup/practice2.js
+++ practice-oop-01-starting-setup/practice2.js
@@ -1,5 +1,5 @@
 class User {
-    constructor(name, borrowedBooks = []) {
+    constructor(name, borrowedBooks = [], ) {
         this.name = name;
         this.borrowedBooks = borrowedBooks;
     }
@@ -34,14 +34,15 @@ class Library {
     constructor(books = [], users = []) {
         this.books = books;
         this.users = users;
+
     }
 
     addBook(book) {
-
+        console.log(book);
     }
 
     addUser(user) {
-
+        console.log(user);
     }
 
     listAvailableBooks() {
@@ -62,13 +63,32 @@ class Logger {
 
 class App {
     constructor() {
+        this.logger = new Logger();
         this.book = new Book();
         this.library = new Library();
         this.user = new User();
+
+        this.book1 = new Book(`The Ministry for the Future`, false, null);
+        this.book2 = new Book(`Where the Crawdads Sing`, false, null);
+        this.book3 = new Book(`Norwegian Wood`, false, null);
+        this.book4 = new Book(`Sapiens: A Brief History of Humankind`, false, null);
+        this.book5 = new Book(`A Gentleman in Moscow`, false, null);
+
+        this.library.addBook(this.book1);
+        this.library.addBook(this.book2);
+        this.library.addBook(this.book3);
+        this.library.addBook(this.book4);
+        this.library.addBook(this.book5);
+
+        this.user1 = new User(`Alice`);
+        this.user2 = new User(`Bok`);
+
+        this.library.addUser(this.user1);
+        this.library.addUser(this.user2);
     }
 
     run() {
-
+        const f1 = this.user.borrowBook(this.book1, this.user1);
     }
 }
 
