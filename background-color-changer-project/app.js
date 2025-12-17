bf42b6bd509bdd307fb665f91257e3a823da6d36
SapereAudePr <capitolsmife@gmail.com>
2025-12-17 16:35:24 +0300

    I made a simple background color changer project

---
+++ background-color-changer-project/app.js
@@ -0,0 +1,71 @@
+const style = document.createElement('style');
+style.innerHTML = `
+.color-buttons {
+width: 100px;
+height: 100px;
+padding: 10px 20px;
+margin: 15px;
+border: none;
+border-radius: 50%;
+cursor: pointer;
+transition: box-shadow 0.5s;
+}
+
+.color-buttons:hover {
+opacity: 0.8;
+}
+
+.ul {
+list-style-type: none;
+padding: 0;
+}
+`
+document.head.appendChild(style);
+
+
+const blackBtnHandler = () => {
+    document.body.style.backgroundColor = 'black';
+}
+
+const redBtnHandler = () => {
+    document.body.style.backgroundColor = 'red';
+}
+
+const whiteBtnHandler = () => {
+    document.body.style.backgroundColor = 'white';
+}
+
+const blackBtn = document.createElement('button');
+blackBtn.className = `color-buttons`;
+blackBtn.style.backgroundColor = 'black';
+blackBtn.addEventListener('click', () => {
+    blackBtnHandler();
+})
+const redBtn = document.createElement('button');
+redBtn.className = `color-buttons`;
+redBtn.style.backgroundColor = 'red';
+redBtn.addEventListener('click', () => {
+        redBtnHandler();
+})
+
+const whiteBtn = document.createElement('button');
+whiteBtn.className = `color-buttons`;
+whiteBtn.style.backgroundColor = 'white';
+whiteBtn.addEventListener('click', () => {
+    whiteBtnHandler();
+})
+
+const sectionEl = document.createElement("section");
+document.body.append(sectionEl);
+const divEl = document.createElement("div");
+sectionEl.append(divEl);
+
+const ulEl = document.createElement("ul");
+divEl.append(ulEl);
+
+ulEl.append(blackBtn);
+ulEl.append(redBtn);
+ulEl.append(whiteBtn);
+
+
+
