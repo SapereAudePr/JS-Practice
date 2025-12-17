2a46f945cc51a28684c101799a2dace7c18d65f7
SapereAudePr <capitolsmife@gmail.com>
2025-12-17 18:31:10 +0300

    p color also changes with button clicks

--- background-color-changer-project/app.js
+++ background-color-changer-project/app.js
@@ -1,10 +1,19 @@
 const style = document.createElement('style');
 style.innerHTML = `
+
+section {
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin-top: 20%;
+}
+
 .color-buttons {
 width: 100px;
 height: 100px;
 padding: 10px 20px;
-margin: 5px;
+margin: 10px;
 border: none;
 border-radius: 50%;
 cursor: pointer;
@@ -15,51 +24,67 @@ transition: box-shadow 0.5s;
 opacity: 0.8;
 border: 2px solid gray;
 transition: box-shadow 0.5s;
-
 }
 
 ul {
-list-style-type: none;
-align-items: center;
-justify-content: center;
 display: flex;
-margin: 20% auto;
+padding: 0;
+margin-top: 20px;
+}
+
+p {
+font-size: 30px;
+font-family: monospace;
+margin: 0;
 }
 `
 
 document.head.appendChild(style);
 
 
-const colorChanger = (color) => {
-    document.body.style.backgroundColor = color;
+const pColorChanger = colour => {
+    p.style.color = colour;
+}
+
+const colorChanger = (bgColour, pColour) => {
+    document.body.style.backgroundColor = bgColour;
+    pColorChanger(pColour);
 }
 
 const blackBtn = document.createElement('button');
 blackBtn.className = `color-buttons`;
 blackBtn.style.backgroundColor = 'black';
 blackBtn.addEventListener('click', () => {
-    colorChanger('black');
+    colorChanger('black', `white`);
 })
 const redBtn = document.createElement('button');
 redBtn.className = `color-buttons`;
 redBtn.style.backgroundColor = 'red';
 redBtn.addEventListener('click', () => {
-    colorChanger(`red`)
+    colorChanger(`red`, `white`)
 })
 const whiteBtn = document.createElement('button');
 whiteBtn.className = `color-buttons`;
 whiteBtn.style.backgroundColor = 'white';
 whiteBtn.addEventListener('click', () => {
-    colorChanger(`white`);
+    colorChanger(`white`, `black`);
 })
 
 const sectionEl = document.createElement("section");
 document.body.append(sectionEl);
+
+const p = document.createElement("p");
+p.textContent = 'Click Me!';
+
+sectionEl.append(p);
+
 const divEl = document.createElement("div");
 sectionEl.append(divEl);
 
 const ulEl = document.createElement("ul");
 divEl.append(ulEl);
+
+
 
 ulEl.append(blackBtn);
 ulEl.append(redBtn);
